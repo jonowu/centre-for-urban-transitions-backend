9c4c1d3e8553d4b4b06334e2c5c40313f271dc32
jonowu <53251695+jonowu@users.noreply.github.com>
2020-07-15 15:56:09 +1000

    feat: add sanity schemas and deskStructure

---
+++ deskStructure.js
@@ -0,0 +1,47 @@
+import S from '@sanity/desk-tool/structure-builder';
+import {
+  FaEnvelope,
+  FaFile,
+  FaHome,
+  FaInfo,
+  FaPencilAlt,
+  FaRegCalendarAlt,
+  FaRegNewspaper,
+  FaUserGraduate,
+} from 'react-icons/fa';
+
+const hiddenTypes = ['homePage', 'aboutPage', 'contactPage'];
+
+export default () =>
+  S.list() // defines the first pane
+    .title('Content')
+    .items([
+      S.listItem()
+        .title('Home Page')
+        .child(S.document().schemaType('homePage').documentId('homePage'))
+        .icon(FaHome),
+      S.listItem()
+        .title('About Us')
+        .child(S.document().schemaType('aboutPage').documentId('aboutPage'))
+        .icon(FaInfo),
+      S.listItem()
+        .title('Our Research')
+        .schemaType('researchPost')
+        .child(S.documentTypeList('researchPost').title('Research posts'))
+        .icon(FaPencilAlt),
+      S.listItem().title('Transitions Resources').icon(FaRegNewspaper),
+      S.listItem().title('PhD Life').icon(FaUserGraduate),
+      S.listItem().title('Events').icon(FaRegCalendarAlt),
+      S.listItem()
+        .title('Contact Us')
+        .child(S.document().schemaType('contactPage').documentId('contactPage'))
+        .icon(FaEnvelope),
+      S.listItem()
+        .title('Single Pages') // creates the settings item in the first pane
+        .schemaType('page')
+        .child(S.documentTypeList('page').title('Pages')),
+      S.divider(),
+      ...S.documentTypeListItems().filter(
+        (listItem) => !hiddenTypes.includes(listItem.getId())
+      ),
+    ]);
---
+++ schemas/homePage.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'homePage',
+  title: 'Home Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+};
